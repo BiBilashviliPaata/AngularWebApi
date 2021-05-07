2aa20820b4a10f738923e56489224aead55187ac
bibilashvili.paata@gmail.com <bibilashvili.paata@gmail.com>
2021-05-07 18:34:26 +0400

    AddAngular

---
+++ AngularWebApi/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Context;
+using DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserModel>>> GetUsers()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        [HttpGet("{Id}")]
+        public ActionResult <UserModel> GetUserById(int id)
+        {
+            return _context.User.Find(id);
+        }
+    }
+}
