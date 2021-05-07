5679d1820029b2cc5556a3e553da48d6776135ae
bibilashvili.paata@gmail.com <bibilashvili.paata@gmail.com>
2021-05-07 16:31:07 +0400

    1

---
+++ DAL/Entities/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
